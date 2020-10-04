460f42fb73909721adba4faa257a3aa460402335
omkar2704 <72335696+omkar2704@users.noreply.github.com>
2020-10-04 04:22:19 -0700

    Add files via upload

---
+++ Thetaprichai/thetaprichai/main.js
@@ -0,0 +1,6 @@
+//Selectors
+let header = document.querySelector('.header');
+let hamburgerMenu = document.querySelector('.hamburger-menu');
+hamburgerMenu.addEventListener('click', function () {
+header.classList.toggle('menu-open');
+})
